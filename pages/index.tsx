d1c2b6035b89529a1b66536163b600a0049c7a7c
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-03 10:59:11 +0200

    feat(home): add dark mode toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from "next/head";
 
+import { DarkToggle } from "../components/DarkToggle/DarkToggle";
+
 export default function Home() {
   return (
-    <Head>
-      <title>MrMarble</title>
-      <meta name="description" content="MrMarble Portfolio" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <main className="container mx-auto">
+      <Head>
+        <title>MrMarble</title>
+        <meta name="description" content="MrMarble Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <DarkToggle size="2rem" className="absolute right-5 top-5" />
+    </main>
   );
 }
